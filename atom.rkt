da2399117e7212f7e01b14734dd3ec45bd18cef6
Ryan Culpepper <ryanc@racket-lang.org>
2011-11-24 22:02:58 -0800

    more work on atom.rkt

--- atom.rkt
+++ atom.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 (require racket/class
          (planet clements/sxml2:1))
+(provide (all-defined-out))
 
 #|
 Atom 1.0
@@ -10,60 +11,77 @@ Goals:
  1) read-only access to Atom documents
  2) creation of new Atom documents
  3) modification ("patching"?) of existing Atom documents
+
+Unlike some other classes in this library, atom% instances are not
+interned, no parent link, etc.
 |#
-
-(define xml%
-  (class object%
-    (init-field sxml)
-    (super-new)
-
-    (define/public (get-children/tag tag)
-      (filter (lambda (c) (pair? c) (eq? tag (car c)))
-              (cdr sxml)))
-
-    (define/public (get-child/tag tag #:who who)
-      (let ([cs (get-children/tag tag)])
-        (and (pair? cs) (car cs))))
-    ))
 
 (define atom<%>
   ;; comprises atom-feed<%>, atom-entry<%>
   (interface ()
-    get-sxml ;; -> SXML
-
+    get-sxml     ;; -> SXML
     get-id       ;; -> string
     get-title    ;; -> string
     get-updated  ;; -> string(date)
     get-link     ;; string -> string
     get-raw-link ;; string -> SXML
+    get-entries  ;; -> (listof atom<%>)
+    get-raw-entries ;; -> (listof SXML)
     ))
 
 (define atom%
   (class* object% (atom<%>)
-    (init sxml)
-    (define-values (root-tag attrs children)
-      (match sxml
-        [(list* tag (cons '@ attrs) children)
-         (values tag attrs children)]
-        [(list* tag children)
-         (values tag null children)]))
+    (init-field sxml)
+
+    (define root
+      (cond [(and (pair? sxml) (memq (car sxml) '(atom:feed atom:entry)))
+             sxml]
+            [else
+             (car* ((sxpath '((*or* atom:feed atom:entry))) sxml))]))
+
+    (unless root
+      (error 'atom% "invalid Atom document: ~e" sxml))
+
     (super-new)
 
+    (define/public (get-sxml) sxml)
     (define/public (get-id)
-      (get1 'atom:id #:who who))
+      (get1 'atom:id))
     (define/public (get-title)
-      (get1 'atom:title #:who who))
+      (get1 'atom:title))
     (define/public (get-updated)
-      (get1 'atom:updated #:who who))
+      (get1 'atom:updated))
     (define/public (get-link rel)
-      ...)
+      (car* ((sxpath `((atom:link (@ rel (equal? ,rel))) @ href *text*)) root))
+      #|
+      (let ([raw-link (get-raw-link rel)])
+        (and raw-link
+             (car* ((sxpath '(@ href *text*)) raw-link))))
+      |#)
     (define/public (get-raw-link rel)
-      ...)
+      (car* ((sxpath `((atom:link (@ rel (equal? ,rel))))) root))
+      #|
+      (car* ((node-join (sxml:child (ntype?? 'atom:link))
+                        (sxml:filter (sxpath `(@ rel (equal? ,rel)))))
+             root))
+      |#)
 
-    (define/private (get1 tag #:who who)
-      ...)
+    (define/public (get-entries)
+      (for/list ([entry (in-list (get-raw-entries))])
+        (new atom% (sxml entry))))
+
+    (define/public (get-raw-entries)
+      ((sxpath '(atom:entry)) root))
+
+    ;; ----
+
+    (define/private (get1 tag)
+      (car* ((sxpath `(,tag *text*)) root)))
 
     ))
+
+(define (car* x)
+  (and (pair? x) (car x)))
 
 #|
   atomFeed =
