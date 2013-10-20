5d0d425fda4e2389d34722b48793ac76fd15ca0a
Ryan Culpepper <ryanc@racket-lang.org>
2013-10-20 01:20:27 -0400

    reorganize as package

--- info.rkt
+++ info.rkt
@@ -1,20 +1,9 @@
-;; Copyright 2011-2012 Ryan Culpepper
+;; Copyright 2013 Ryan Culpepper
 ;; Released under the terms of the LGPL version 3 or later.
 ;; See the file COPYRIGHT for details.
 
-#lang setup/infotab
+#lang info
 
-(define name "webapi")
-(define scribblings '(("scribblings/webapi.scrbl" (multi-page))))
-
-(define blurb
-  '("Rudimentary implementations of a few web APIs, "
-    "including OAuth2, PicasaWeb, and Blogger."))
-(define categories '(io net xml))
-(define can-be-loaded-with 'all)
-(define primary-file "main.rkt")
-(define required-core-version "5.3.4")
-(define repositories '("4.x"))
-
-(define release-notes
-  '("Fixed problem compiling docs."))
+(define collection 'multi)
+(define deps '("base"))
+(define build-deps '("racket-doc" "scribble-lib"))
---
+++ webapi/info.rkt
@@ -0,0 +1,8 @@
+;; Copyright 2011-2013 Ryan Culpepper
+;; Released under the terms of the LGPL version 3 or later.
+;; See the file COPYRIGHT for details.
+
+#lang setup/infotab
+
+(define name "webapi")
+(define scribblings '(("scribblings/webapi.scrbl" (multi-page))))
