24c8d9ebae7486481fd997e1a23c946e86e95363
Jin-Ho King <kjinho@users.noreply.github.com>
2022-02-11 15:56:42 -0500

    allow option to change redirect-uri

--- webapi/private/oauth2-web.rkt
+++ webapi/private/oauth2-web.rkt
@@ -20,36 +20,42 @@ TO DO:
  - try oauth2/request-access/browser again
 |#
 
-(define (oauth2/request-auth-code/browser auth-server client scopes)
+(define (oauth2/request-auth-code/browser auth-server client scopes 
+         #:redirect-uri [redirect-uri "http://localhost:8000/oauth2/response"]
+         #:port [port 8000])
   (let ([oauth2 (new oauth2%
                      (auth-server auth-server)
                      (client client))])
-    (let ([auth-code (request-auth-code/web oauth2 scopes)])
+    (let ([auth-code (request-auth-code/web oauth2 scopes 
+                      #:port port 
+                      #:redirect-uri redirect-uri)])
       (send oauth2 acquire-token/auth-code!
             auth-code
-            #:redirect-uri "http://localhost:8000/oauth2/response"
+            #:redirect-uri redirect-uri
             #:who 'oauth2/request-access/web)
       oauth2)))
 
 (define (request-auth-code/web oauth2 scopes
-                               #:who [who 'request-access/web])
+                               #:who [who 'request-access/web]
+                               #:port [port 8000]
+                               #:redirect-uri [redirect-uri "http://localhost:8000/oauth2/response"])
   (let ([chan (make-channel)]
         [server-cust (make-custodian)])
     (parameterize ((current-custodian server-cust))
       (thread
        (lambda ()
-         (serve/servlet (make-servlet oauth2 scopes chan)
+         (serve/servlet (make-servlet oauth2 scopes chan #:redirect-uri redirect-uri)
                         #:launch-browser? #t
                         #:quit? #t
                         #:banner? #f
-                        #:port 8000
+                        #:port port
                         #:servlet-path "/oauth2/init"
                         #:servlet-regexp #rx"^/oauth2/"
                         #:extra-files-paths null))))
     (begin0 (channel-get chan)
       (custodian-shutdown-all server-cust))))
 
-(define (make-servlet oauth2 scopes chan)
+(define (make-servlet oauth2 scopes chan #:redirect-uri [redirect-uri "http://localhost:8000/oauth2/response"])
   (lambda (req)
     (let ([path (string-join (map path/param-path (url-path (request-uri req))) "/")])
       (cond [(equal? path "oauth2/init")
@@ -59,7 +65,7 @@ TO DO:
                 (send auth-server get-auth-request-url
                       #:client client
                       #:scopes scopes
-                      #:redirect-uri "http://localhost:8000/oauth2/response")))]
+                      #:redirect-uri redirect-uri)))]
             [(equal? path "oauth2/response")
              (let ([bindings (request-bindings/raw req)])
                (cond [(bindings-assq #"code" bindings)
